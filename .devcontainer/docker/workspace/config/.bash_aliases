85b34e4c019927a9af5d550fd2616fcde5326759
Junichi Shibata <rain.noise@gmail.com>
2023-05-23 17:43:51 +0900

    Dev Container 導入

---
+++ .devcontainer/docker/workspace/config/.bash_aliases
@@ -0,0 +1,8 @@
+# =========================================================
+# Here is an alias that is convenient for development.
+# =========================================================
+alias ll='ls -l'
+alias la='ls -A'
+alias php-cs-fixer="~/.composer/vendor/bin/php-cs-fixer"
+alias pcf="php-cs-fixer"
+alias pcf-f="pcf fix --config=/workspace/.php-cs-fixer.dist.php"
---
+++ .devcontainer/post_create_command.sh
@@ -0,0 +1,13 @@
+echo "============================================================"
+echo " Set Up Dev Container ..."
+echo "============================================================"
+echo ""
+echo "> Install php-cs-fixer"
+composer global require "friendsofphp/php-cs-fixer:3.17.0" --dev
+echo ">> Done."
+
+echo ""
+echo "============================================================"
+echo " Dev Container Was Ready"
+echo "============================================================"
+echo ""

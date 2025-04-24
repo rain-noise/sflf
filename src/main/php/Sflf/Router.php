713f69cef9fb0403f96a6c3e09577f563da1aa81
Junichi Shibata <rain.noise@gmail.com>
2025-04-24 14:54:16 +0900

    fix: 名前空間の階層区切り付与挙動を変更

--- src/main/php/Sflf/Router.php
+++ src/main/php/Sflf/Router.php
@@ -37,7 +37,7 @@
  * }
  *
  * @package   SFLF
- * @version   v4.0.0
+ * @version   v4.1.0
  * @author    github.com/rain-noise
  * @copyright Copyright (c) 2017 github.com/rain-noise
  * @license   MIT License https://github.com/rain-noise/sflf/blob/master/LICENSE
@@ -176,7 +176,7 @@ class Router
      */
     private function _getControllerName()
     {
-        return $this->controller_class_namespace . $this->_camelize($this->getPartOfController()) . $this->controller_class_name_suffix ;
+        return $this->controller_class_namespace.'\\'.$this->_camelize($this->getPartOfController()).$this->controller_class_name_suffix ;
     }
 
     /**
